41a9c5d77920ab01ce187d14708a9d35241a7690
gizawsim <gizawsim@gmail.com>
2022-08-29 15:09:13 +0300

    some filess deleted

--- fetching.js
+++
@@ -1,2 +0,0 @@
-console.log("This API Fetching is used to fetch list of dogs");
-console.log(fetch("https://dog.ceo/api/breeds/list/all"));
--- my.js
+++
@@ -1,9 +0,0 @@
-var myName = "Giz";
-
-let myFullName = "Gizaw Simeon";
-
-const pi = 3.14;
-
-console.log(myName)
-console.log(myFullName)
-console.log(pi)
